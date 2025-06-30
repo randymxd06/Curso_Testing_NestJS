ebd2d98116fb1fffaae7c906f7d840968cc03003
randyhytech <randy.martinez@hytechhome.com>
2025-06-30 13:38:20 -0400

    feat: :sparkles: I finish the findAll method

--- 01 - Pokemon App/src/pokemons/pokemons.service.ts
+++ 01 - Pokemon App/src/pokemons/pokemons.service.ts
@@ -2,20 +2,47 @@ import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { PaginationDto } from 'src/shared/dtos/pagination.dto';
+import { PokeApiResponse } from './interfaces/pokeapi.response';
+import { PokeAPIPokemonResponse } from './interfaces/pokeapi-pokemon.response';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonsService {
+
+  paginatedPokemonCache = new Map<string, Pokemon[]>();
+
   create(createPokemonDto: CreatePokemonDto) {
     return 'This action adds a new pokemon';
   }
 
-  async findAll(paginationDto: PaginationDto) {
+  async findAll(paginationDto: PaginationDto): Promise<Pokemon[]> {
+    
     const { limit = 10, page = 1 } = paginationDto;
     const offset = (page - 1) * limit;
+
+    const cacheKey = `${limit}-${offset}`;
+
+    if(this.paginatedPokemonCache.has(cacheKey)) {
+      return this.paginatedPokemonCache.get(cacheKey);
+    }
+    
     const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
     const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    
+    const data = (await response.json()) as PokeApiResponse;
+
+    const pokemonPromises = data.results.map(pokemon => {
+      const url = pokemon.url;
+      const id = url.split('/').slice(-2)[0]
+      return this.getPokemonInformation(+id);
+    });
+
+    const pokemons = await Promise.all(pokemonPromises);
+    
+    this.paginatedPokemonCache.set(cacheKey, pokemons);
+
+    return pokemons;
+
   }
 
   findOne(id: number) {
@@ -29,4 +56,24 @@ export class PokemonsService {
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
   }
+
+  private async getPokemonInformation(id:  number): Promise<Pokemon> {
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+    const data = (await response.json()) as PokeAPIPokemonResponse;
+
+    return {
+      id: data.id,
+      name: data.name,
+      type: data.types[0].type.name,
+      hp: data.stats[0].base_stat,
+      sprites: [
+        data.sprites.front_default,
+        data.sprites.back_default
+      ]
+    }
+
+  }
+
 }
